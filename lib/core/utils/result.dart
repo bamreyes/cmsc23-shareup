b12ed4412cf8994360aa0161a885cb31ffea9c1b
John Abraham Reyes <jbreyes24@up.edu.ph>
2026-04-22 23:26:22 +0800

    feat: add generic Result class for success/error handling

---
+++ lib/core/utils/result.dart
@@ -0,0 +1,15 @@
+class Result<T> {
+  final T? data;
+  final String? error;
+  final bool isSuccess;
+
+  Result.success(this.data)
+      : error = null,
+        isSuccess = true;
+
+  Result.error(this.error)
+      : data = null,
+        isSuccess = false;
+
+  bool get isError => !isSuccess;
+}
